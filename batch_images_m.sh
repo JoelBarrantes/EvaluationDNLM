0a5d868c10c3e00d5a6367bdb4f97ec6944a9bb4
Ubuntu <jdnlm@jdnlm3.jdnlm36202.j8.internal.cloudapp.net>
2018-03-14 04:39:54 +0000

    Results for manual s.

---
+++ batch_images_m.sh
@@ -0,0 +1,89 @@
+java -Djava.library.path=x64 -jar sa.jar 1 7 3 180 3 >> results/dataset_dice_1m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 2 7 3 180 3 >> results/dataset_dice_2m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 3 7 3 180 3 >> results/dataset_dice_3m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 4 7 3 180 3 >> results/dataset_dice_4m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 5 7 3 180 3 >> results/dataset_dice_5m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 6 7 3 180 3 >> results/dataset_dice_6m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 7 7 3 180 3 >> results/dataset_dice_7m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 8 7 3 180 3 >> results/dataset_dice_8m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 9 7 3 180 3 >> results/dataset_dice_9m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 10 7 3 180 3 >> results/dataset_dice_10m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 11 7 3 180 3 >> results/dataset_dice_11m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 12 7 3 180 3 >> results/dataset_dice_12m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 13 7 3 180 3 >> results/dataset_dice_13m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 14 7 3 180 3 >> results/dataset_dice_14m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 15 7 3 180 3 >> results/dataset_dice_15m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 16 7 3 180 3 >> results/dataset_dice_16m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 17 7 3 180 3 >> results/dataset_dice_17m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 18 7 3 180 3 >> results/dataset_dice_18m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 19 7 3 180 3 >> results/dataset_dice_19m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 20 7 3 180 3 >> results/dataset_dice_20m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 21 7 3 180 3 >> results/dataset_dice_21m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 22 7 3 180 3 >> results/dataset_dice_22m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 23 7 3 180 3 >> results/dataset_dice_23m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 24 7 3 180 3 >> results/dataset_dice_24m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 25 7 3 180 3 >> results/dataset_dice_25m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 26 7 3 180 3 >> results/dataset_dice_26m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 27 7 3 180 3 >> results/dataset_dice_27m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 28 7 3 180 3 >> results/dataset_dice_28m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 29 7 3 180 3 >> results/dataset_dice_29m.txt
+sleep 10
+
+java -Djava.library.path=x64 -jar sa.jar 30 7 3 180 3 >> results/dataset_dice_30m.txt
+sleep 10
